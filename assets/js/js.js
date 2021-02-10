64d83d31288eff44924556a0703a5c287a505629
rorwey <rorwey@foxmail.com>
2021-02-10 18:47:12 +0800

    Update js.js

--- assets/js/js.js
+++ assets/js/js.js
@@ -28,8 +28,8 @@ function loading() {
 
 window.onload=function(){
 	getYear();
-	console.log(document.getElementsByClassName('loading')[0].style.display);
-	if(document.getElementsByClassName('loading')[0].style.display == 'block'){
+	console.log(document.getElementsByClassName('loading')[0].style.display|| '');
+	if(document.getElementsByClassName('loading')[0].style.display == 'block'||document.getElementsByClassName('loading')[0].style.display == 'block' ''){
 		console.log("test");
 		loading();
 	}
